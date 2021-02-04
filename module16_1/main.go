07b94f9afb739a1c30b972089804135c61143e58
leejoys <44557202+leejoys@users.noreply.github.com>
2021-02-04 05:44:04 +0200

    16_1 done

--- module16_1/main.go
+++ module16_1/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func reporter(i int) {
+	for {
+		time.Sleep(time.Second * 1)
+		fmt.Printf("Goroutine %d reporting\n", i)
+	}
+}
+
+func main() {
+	var n int
+	fmt.Println("Enter number of goroutines:")
+	fmt.Scanln(&n)
+	for i := 1; i <= n; i++ {
+		go reporter(i)
+	}
+	fmt.Scanln()
+}
