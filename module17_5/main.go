b88152987ffa447279f6cdff42a08a996a80a9f3
leejoys <44557202+leejoys@users.noreply.github.com>
2021-02-09 01:27:55 +0200

    17_5 done

--- module17_5/main.go
+++ module17_5/main.go
@@ -1,21 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 //Напишите программу, которая делает следующее: одна горутина по порядку отсылает числа от 1 до 100 в канал,
 //вторая горутина их принимает в правильном порядке и печатает на экран (в консоль).
 
 var intChan chan int
 
 func sender(c chan<- int) {
-
-}
-
-func receiver(c <-chan int) {
-
+	for i := 1; i <= 100; i++ {
+		c <- i
+	}
+	close(c)
 }
 
 func main() {
+	var wg sync.WaitGroup
 	intChan := make(chan int, 0)
+	wg.Add(1)
 
 	go sender(intChan)
-	go receiver(intChan)
+
+	go func(c <-chan int) {
+		for i := range c {
+			fmt.Println(i)
+		}
+		wg.Done()
+	}(intChan)
+
+	wg.Wait()
 }
