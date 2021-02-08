98c1a67c940a5164f15d1b833f9044e1ea708f74
leejoys <44557202+leejoys@users.noreply.github.com>
2021-02-08 05:57:16 +0200

    17_2_v2

--- module17_2/main.go
+++ module17_2/main.go
@@ -1,3 +1,49 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// )
+
+// // Шаг наращивания счётчика
+// const step int64 = 1
+
+// // Конечное значение счетчика
+// const endCounterValue int64 = 1000
+
+// func main() {
+
+// 	var counter int64 = 0
+// 	var c = sync.NewCond(&sync.Mutex{})
+// 	increment := func() {
+// 		c.L.Lock()
+// 		counter += step
+// 		c.L.Unlock()
+// 	}
+
+// 	var iterationCount int = int(endCounterValue / step)
+// 	for i := 1; i <= iterationCount; i++ {
+// 		go increment()
+// 	}
+
+// 	go func() {
+// 		for {
+// 			c.L.Lock()
+// 			if counter >= endCounterValue {
+// 				c.Signal()
+// 			}
+// 			c.L.Unlock()
+// 		}
+// 	}()
+
+// 	// Ожидаем поступления сигнала
+// 	c.L.Lock()
+// 	c.Wait()
+// 	c.L.Unlock()
+// 	// Печатаем результат, надеясь, что будет 1000
+// 	fmt.Println(counter)
+// }
+
 package main
 
 import (
@@ -14,11 +60,15 @@ const endCounterValue int64 = 1000
 func main() {
 
 	var counter int64 = 0
-	var c = sync.NewCond(&sync.RWMutex{})
+	var wg sync.WaitGroup
+	var c = sync.NewCond(&sync.Mutex{})
 	increment := func() {
-		c.L.Lock()
-		counter += step
-		c.L.Unlock()
+		for {
+			c.L.Lock()
+			counter += step
+			c.Wait()
+			c.L.Unlock()
+		}
 	}
 
 	var iterationCount int = int(endCounterValue / step)
@@ -26,20 +76,23 @@ func main() {
 		go increment()
 	}
 
+	wg.Add(1)
 	go func() {
+
 		for {
 			c.L.Lock()
-			if counter >= endCounterValue {
+			if counter < endCounterValue {
 				c.Signal()
+			} else if counter >= endCounterValue {
+				wg.Done()
+				return
 			}
 			c.L.Unlock()
 		}
 	}()
 
 	// Ожидаем поступления сигнала
-	c.L.Lock()
-	c.Wait()
-	c.L.Unlock()
+	wg.Wait()
 	// Печатаем результат, надеясь, что будет 1000
 	fmt.Println(counter)
 }
