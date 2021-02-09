1f3d3a6938e053eab3d2ef52e94b0cea0010aa08
leejoys <44557202+leejoys@users.noreply.github.com>
2021-02-09 02:21:06 +0200

    17_4?

--- module17_4/main.go
+++ module17_4/main.go
@@ -1,6 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 //Напишите код, в котором имеются два канала сообщений из целых чисел так,
 //чтобы приём сообщений всегда приводил к блокировке.
 //Приёмом сообщений из обоих каналов будет заниматься главная горутина. Сделайте так, чтобы во время такого
 //«бесконечного ожидания» сообщений выполнялась фоновая работа в виде вывода текущего времени в консоль.
+
+func main() {
+
+	evenChan, oddChan := make(chan int, 0), make(chan int, 0)
+
+	go func() {
+		for i := 1; i <= 100; i++ {
+			time.Sleep(time.Second / 4)
+			if i%2 != 0 {
+				oddChan <- i
+			} else {
+				evenChan <- i
+			}
+		}
+		close(evenChan)
+		close(oddChan)
+	}()
+
+	for {
+
+		select {
+		case _, notclosed := <-evenChan:
+			if !notclosed {
+				break
+			} else {
+				fmt.Println("Even!")
+			}
+		case _, notclosed := <-oddChan:
+			if !notclosed {
+				break
+			} else {
+				fmt.Println("Odd!")
+			}
+		case <-time.Tick(time.Second / 10):
+			fmt.Println(time.Now())
+		}
+	}
+}
