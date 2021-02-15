60b736a10c07c46f611698a475fa1612b12f5376
leejoys <44557202+leejoys@users.noreply.github.com>
2021-02-15 11:32:47 +0200

    17_3 done

--- module17_3/main.go
+++ module17_3/main.go
@@ -1,6 +1,54 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 //Напишите код, в котором имеются два канала сообщений из целых чисел, так,
 //чтобы приём сообщений из них никогда не приводил к блокировке
 //и чтобы вероятность приёма сообщения из первого канала была выше в 2 раза, чем из второго.
 //*Если хотите, можете написать код, который бы демонстрировал это соотношение.
+
+func main() {
+
+	var sumOfOne int
+	var sumOfTwo int
+
+	oneChan, twoChan := make(chan int, 0), make(chan int, 0)
+	close(oneChan)
+	close(twoChan)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		fmt.Scanln()
+		wg.Done()
+	}()
+
+	go func() {
+		for {
+			<-time.Tick(time.Second / 4)
+			fmt.Println(float64(sumOfTwo) / float64(sumOfOne))
+		}
+	}()
+
+	go func() {
+		for {
+
+			select {
+			case <-oneChan:
+				sumOfOne++
+
+			case <-twoChan:
+				sumOfTwo++
+
+			case <-twoChan:
+				sumOfTwo++
+			}
+		}
+	}()
+	wg.Wait()
+}
