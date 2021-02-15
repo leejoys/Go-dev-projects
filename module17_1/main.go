512e2e2243e56bff0982d9821dcddc8e6969fd5b
leejoys <44557202+leejoys@users.noreply.github.com>
2021-02-15 08:32:52 +0200

    17_1 nice

--- module17_1/main.go
+++ module17_1/main.go
@@ -6,8 +6,11 @@ import (
 	"sync/atomic"
 )
 
-// Шаг наращивания счётчика
-const step int64 = 100
+// Количество горутин
+const workers int64 = 10
+
+// Шаг наращивания счетчика
+const step int64 = 2
 
 // Конечное значение счетчика
 const endCounterValue int64 = 1000
@@ -16,18 +19,24 @@ func main() {
 
 	var counter int64 = 0
 	var wg sync.WaitGroup
-	increment := func() {
-		defer wg.Done()
-		atomic.AddInt64(&counter, step)
-	}
+
 	// Не всегда вычисление этой переменной будет приводить к верному
 	// результату в счётчике, но для правильных значений
 	// и для удобства - можно
-	var iterationCount int = int(endCounterValue / step)
-	for i := 1; i <= iterationCount; i++ {
+	var iterationCount int = int(endCounterValue / workers / step)
+
+	increment := func() {
+		defer wg.Done()
+		for i := 0; i < iterationCount; i++ {
+			atomic.AddInt64(&counter, step)
+		}
+	}
+
+	for i := 1; i <= int(workers); i++ {
 		wg.Add(1)
 		go increment()
 	}
+
 	// Ожидаем поступления сигнала
 	wg.Wait()
 	// Печатаем результат, надеясь, что будет 1000
