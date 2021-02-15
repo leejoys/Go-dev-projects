061c641ef576afb99c4e055b5bef18984ada49aa
leejoys <44557202+leejoys@users.noreply.github.com>
2021-02-15 08:47:53 +0200

    17_2 nice

--- module17_2/main.go
+++ module17_2/main.go
@@ -1,49 +1,3 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"sync"
-// )
-
-// // Шаг наращивания счётчика
-// const step int64 = 1
-
-// // Конечное значение счетчика
-// const endCounterValue int64 = 1000
-
-// func main() {
-
-// 	var counter int64 = 0
-// 	var c = sync.NewCond(&sync.Mutex{})
-// 	increment := func() {
-// 		c.L.Lock()
-// 		counter += step
-// 		c.L.Unlock()
-// 	}
-
-// 	var iterationCount int = int(endCounterValue / step)
-// 	for i := 1; i <= iterationCount; i++ {
-// 		go increment()
-// 	}
-
-// 	go func() {
-// 		for {
-// 			c.L.Lock()
-// 			if counter >= endCounterValue {
-// 				c.Signal()
-// 			}
-// 			c.L.Unlock()
-// 		}
-// 	}()
-
-// 	// Ожидаем поступления сигнала
-// 	c.L.Lock()
-// 	c.Wait()
-// 	c.L.Unlock()
-// 	// Печатаем результат, надеясь, что будет 1000
-// 	fmt.Println(counter)
-// }
-
 package main
 
 import (
@@ -60,15 +14,11 @@ const endCounterValue int64 = 1000
 func main() {
 
 	var counter int64 = 0
-	var wg sync.WaitGroup
 	var c = sync.NewCond(&sync.Mutex{})
 	increment := func() {
-		for {
-			c.L.Lock()
-			counter += step
-			c.Wait()
-			c.L.Unlock()
-		}
+		c.L.Lock()
+		counter += step
+		c.L.Unlock()
 	}
 
 	var iterationCount int = int(endCounterValue / step)
@@ -76,23 +26,77 @@ func main() {
 		go increment()
 	}
 
-	wg.Add(1)
 	go func() {
-
 		for {
 			c.L.Lock()
-			if counter < endCounterValue {
+			if counter >= endCounterValue {
 				c.Signal()
-			} else if counter >= endCounterValue {
-				wg.Done()
-				return
 			}
 			c.L.Unlock()
 		}
 	}()
 
 	// Ожидаем поступления сигнала
-	wg.Wait()
+	c.L.Lock()
+	c.Wait()
+	c.L.Unlock()
 	// Печатаем результат, надеясь, что будет 1000
 	fmt.Println(counter)
 }
+
+// Вариант кода с бесконечным приращением и остановкой по проверке значения
+// (почему-то не всегда работает)
+// ==========================================================================
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// )
+
+// // Шаг наращивания счётчика
+// const step int64 = 1
+
+// // Конечное значение счетчика
+// const endCounterValue int64 = 1000
+
+// func main() {
+
+// 	var counter int64 = 0
+// 	var wg sync.WaitGroup
+// 	var c = sync.NewCond(&sync.Mutex{})
+// 	increment := func() {
+// 		for {
+// 			c.L.Lock()
+// 			counter += step
+// 			c.Wait()
+// 			c.L.Unlock()
+// 		}
+// 	}
+
+// 	var iterationCount int = int(endCounterValue / step)
+// 	for i := 1; i <= iterationCount; i++ {
+// 		go increment()
+// 	}
+
+// 	wg.Add(1)
+// 	go func() {
+
+// 		for {
+// 			c.L.Lock()
+// 			if counter < endCounterValue {
+// 				c.Signal()
+// 			} else if counter >= endCounterValue {
+// 				wg.Done()
+// 				return
+// 			}
+// 			c.L.Unlock()
+// 		}
+// 	}()
+
+// 	// Ожидаем поступления сигнала
+// 	wg.Wait()
+// 	// Печатаем результат, надеясь, что будет 1000
+// 	fmt.Println(counter)
+// }
